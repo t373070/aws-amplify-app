0499d4399fa45417f1aff9694ea0efc9d8d1c1af
root <root@ip-192-168-67-5.ap-northeast-1.compute.internal>
2022-07-17 06:41:33 +0000

    mod

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Amplify } from "aws-amplify";
-import { withAuthenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
+import logo from './logo.svg';
+import './App.css';
 
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
-
-function App({ signOut, user }) {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Hello React with AWS</h2>
-        {user ? (
-          <>
-            <h3>私は権限を持っています：{user.username}</h3>
-            <button onClick={signOut}>Sign out</button>
-          </>
-        ) : (
-          <h3>権限がありません</h3>
-        )}
+        <h2>Hello React app with AWS org2</h2>  
       </header>
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
